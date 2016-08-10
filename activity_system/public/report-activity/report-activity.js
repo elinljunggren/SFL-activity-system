7ded610350cefee1129e30b0c509509fa839dd6e
siMs0n <simon.c.nielsen@gmail.com>
2016-08-10 19:02:53 +0200

    Added more fields in report activity

--- activity_system/public/report-activity/report-activity.js
+++ activity_system/public/report-activity/report-activity.js
@@ -8,15 +8,21 @@
     .module('activity_system.report-activity',['ngRoute','ngMaterial', 'ngMessages'])
     .controller('ReportActivityController', ReportActivityController);
 
-  ReportActivityController.$inject = ['$scope'];
+  ReportActivityController.$inject = ['$scope','BackendService'];
 
   /* @ngInject */
-  function ReportActivityController($scope) {
+  function ReportActivityController($scope, BackendService) {
+    var self = this;
     $scope.controller = this;
 
-    var activity = {
-      date: undefined
+    var today = new Date();
+    self.activityReport = {
+      staff: undefined,
+      date: today
     };
+
+    self.selectedStaff = null;
+    self.searchStaff = null;
 
     activate();
 
@@ -24,7 +30,27 @@
 
     function activate() {
       console.log("Activate in report activity!");
+      $scope.data = BackendService.data;
+      console.log($scope.data);
     }
+
+    self.querySearchStaff = function(query) {
+      console.log("query search");
+      return query ? $scope.data.staff.filter(createFilter(query)) : [];
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilter(query) {
+      return function filterFn(object) {
+        return (object.name.indexOf(query) === 0);
+      };
+    }
+
+    self.setStaffName = function () {
+      self.activityReport.staff = self.selectedStaff.name;
+    };
 
   }
 
