23e59472d5d8b114875de40db11342db465b995b
siMs0n <simon.c.nielsen@gmail.com>
2016-09-11 02:54:52 +0200

    Fixed more filters + added program and acategory as headers

--- activity_system/public/admin/admin.js
+++ activity_system/public/admin/admin.js
@@ -7,23 +7,49 @@
     .module('activity_system.admin',['ngRoute','ngMaterial','md.data.table', 'angularMoment'])
     .controller('AdminController', AdminController)
     .filter('matchFilters', function(){
-      return function(areports, filter){
+      return function(areports, filter, data){
         var filteredAReports = [];
         for(var i = 0; i < areports.length; i++){
                 filteredAReports.push(areports[i])
           }
-        console.log(filter);
         var fromDate = moment(filter.fromDate);
         var toDate = moment(filter.toDate);
         for(var i = areports.length-1; i >= 0; i--){
           var areportsDate = moment(areports[i].date);
-            if(filter.schools && filter.schools.length>0 && filter.schools.indexOf(areports[i].school) == -1){
-                filteredAReports.splice(i,1);
-            }else if(filter.fromDate && areportsDate.isBefore(fromDate)){
-                filteredAReports.splice(i,1);
-            }else if(filter.toDate && areportsDate.isAfter(toDate)){
-                filteredAReports.splice(i,1);
+          for(var j = 0; j<data.schools.length; j++){
+            if(data.schools[j].name == areports[i].school){
+              var area = data.schools[j].area;
             }
+          }
+          for(var j = 0; j<data.activities.length; j++){
+            if(data.activities[j].name == areports[i].activity){
+              var acategory = data.activities[j].category;
+            }
+          }
+          for(var j = 0; j<data.programs.length; j++){
+            if(data.activitycategories[j].name == acategory){
+              var program = data.activitycategories[j].program;
+            }
+          }
+          console.log("program", program);
+          console.log("category", acategory);
+          if(filter.schools && filter.schools.length>0 && filter.schools.indexOf(areports[i].school) == -1){
+              filteredAReports.splice(i,1);
+          }else if(filter.fromDate && areportsDate.isBefore(fromDate)){
+              filteredAReports.splice(i,1);
+          }else if(filter.toDate && areportsDate.isAfter(toDate)){
+              filteredAReports.splice(i,1);
+          }else if(filter.areas && filter.areas.length>0 && filter.areas.indexOf(area) == -1){
+              filteredAReports.splice(i,1);
+          }else if(filter.staff && filter.staff.length>0 && filter.staff.indexOf(areports[i].staff) == -1){
+              filteredAReports.splice(i,1);
+          }else if(filter.programs && filter.programs.length>0 && filter.programs.indexOf(program) == -1){
+              filteredAReports.splice(i,1);
+          }else if(filter.activitycategories && filter.activitycategories.length>0 && filter.activitycategories.indexOf(acategory) == -1){
+              filteredAReports.splice(i,1);
+          }else if(filter.activities && filter.activities.length>0 && filter.activities.indexOf(areports[i].activity) == -1){
+              filteredAReports.splice(i,1);
+          }
         }
         return filteredAReports;
       }
@@ -62,9 +88,7 @@
       options : [5,10,25,100]
     };
 
-    $scope.filter = {
-
-    };
+    $scope.filter = {};
 
     $scope.findArea = function(school){
       for(var i = 0; i<$scope.data.schools.length; i++){
@@ -74,8 +98,28 @@
       }
     }
 
+    $scope.findActivityCategory = function(activity){
+      for(var i = 0; i<$scope.data.activities.length; i++){
+        if($scope.data.activities[i].name == activity){
+          return $scope.data.activities[i].category;
+        }
+      }
+    }
+
+    $scope.findProgram = function(acategory){
+      for(var i = 0; i<$scope.data.activitycategories.length; i++){
+        if($scope.data.activitycategories[i].name == acategory){
+          return $scope.data.activitycategories[i].program;
+        }
+      }
+    }
+
     $scope.filterApplied = function(){
       console.log($scope.filter);
+    }
+
+    $scope.clearAllFilters = function(){
+      $scope.filter = {};
     }
 
     function activate() {
